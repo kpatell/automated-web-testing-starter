adf5a8c0efd93994932793b44acc3542ebb5978d
krishanpatel <krishanpatel00@gmail.com>
2021-05-08 16:24:18 -0400

    cleanup and fix launch

--- src/main/java/org/jsk/automation/HelloSelenium.java
+++ src/main/java/org/jsk/automation/HelloSelenium.java
@@ -5,19 +5,24 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HelloSelenium {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
-        WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+        ChromeOptions options = new ChromeOptions().addArguments("--incognito");
+
+        String chromeDriverPath = "build/resources/main/chromedriver";
+        System.setProperty("webdriver.chrome.driver", chromeDriverPath);
+
+        WebDriver driver = new ChromeDriver(options);
+         WebDriverWait wait = new WebDriverWait(driver, 10);
         try {
             driver.get("https://google.com/ncr");
             driver.findElement(By.name("q")).sendKeys("cheese" + Keys.ENTER);
-            WebElement firstResult = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h3")));
-            System.out.println(firstResult.getAttribute("textContent"));
+             WebElement firstResult = wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("h3")));
+             System.out.println(firstResult.getAttribute("textContent"));
         } finally {
             driver.quit();
         }
